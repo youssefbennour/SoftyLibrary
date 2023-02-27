a94de3545cde298cbced91a75d7488347a959f67
youssefbennour <youssefbennour15@gmail.com>
2023-02-27 16:25:24 +0100

    add author repository

---
+++ BookLibrary/Models/AuthorRepository.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BookLibrary.Models
+{
+    public class AuthorRepository: IAuthorRepository
+    {
+        private readonly BookAppDbContext _context;
+        public AuthorRepository(BookAppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<Author>> GetAllAsync()
+        {
+            return await _context.Authors.Include(author => author.Books).ToListAsync();
+        }
+
+        public async Task<int> GetAuthorByBookAsync(string authorName, string bookName)
+        {
+            var book =  await _context.Books.Include(book => book.Author).Where(book => book.Author.Name == authorName).AsNoTracking().FirstOrDefaultAsync();
+            if (book == null) return -1;
+            return book.Author.Id;
+        }
+
+        public Task<Author?> GetAuthorByIdAsync(int? id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> SaveChangesAsync()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ BookLibrary/Models/IAuthorRepository.cs
@@ -0,0 +1,11 @@
+﻿namespace BookLibrary.Models
+{
+    public interface IAuthorRepository
+    {
+        Task<IEnumerable<Author>> GetAllAsync();
+        Task<int> GetAuthorByBookAsync(string AuthorName, string bookName);
+        Task<Author?> GetAuthorByIdAsync(int? id);
+        Task<bool> SaveChangesAsync();
+
+    }
+}
